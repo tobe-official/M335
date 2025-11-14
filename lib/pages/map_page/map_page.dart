4d1cdfb1cafbf989fa3516c2ef9bc314ec6f2f25
Raphael Schreiber (IT-PTR-CEN1-BDE22-Extern) <raphael_mathieu.schreiber@sbb.ch>
2025-11-14 14:28:32 +0100

    fix permission request

--- lib/pages/map_page/map_page.dart
+++ lib/pages/map_page/map_page.dart
@@ -1,12 +1,12 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
-import 'package:WalkeRoo/global_widgets/custom_navigation_bar.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:latlong2/latlong.dart';
 import 'package:flutter_map_tile_caching/flutter_map_tile_caching.dart';
+import 'package:WalkeRoo/global_widgets/custom_navigation_bar.dart';
 import 'package:WalkeRoo/controller/tracking_controller.dart';
 
 class MapPage extends StatefulWidget {
@@ -18,14 +18,15 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   final MapController _mapController = MapController();
+
   static const mapTilerKey = '';
   static const _urlTemplate = 'https://api.maptiler.com/maps/base-v4/{z}/{x}/{y}.png?key=$mapTilerKey';
 
   LatLng? _currentPosition;
   String _locationName = 'Loading location...';
+
   late final FMTCStore _store;
 
-  final List<LatLng> _routePoints = [];
   StreamSubscription<Position>? _positionStreamSub;
   DateTime? _lastLocationUpdate;
 
@@ -33,51 +34,68 @@ class _MapPageState extends State<MapPage> {
   void initState() {
     super.initState();
     _store = FMTCStore('mapStore')..manage.create();
-    _determinePosition();
 
-    // every 0.1 seconds we track the position
-    Timer.periodic(const Duration(milliseconds: 100), (_) {
-      if (mounted && TrackingController().isTracking) setState(() {});
-    });
+    _init();
   }
 
-  Future<void> _determinePosition() async {
-    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      setState(() => _locationName = 'GPS blocked');
+  Future<void> _init() async {
+    final ok = await _ensureLocationPermission();
+    if (!ok) {
+      setState(() => _locationName = "Location permission required");
       return;
     }
 
-    LocationPermission permission = await Geolocator.checkPermission();
+    final pos = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+    _updateLocation(pos);
+
+    _positionStreamSub = Geolocator.getPositionStream(
+      locationSettings: const LocationSettings(accuracy: LocationAccuracy.high, distanceFilter: 3),
+    ).listen(_updateLocation);
+  }
+
+  Future<bool> _ensureLocationPermission() async {
+    final serviceEnabled = await Geolocator.isLocationServiceEnabled();
+
+    if (!serviceEnabled) {
+      setState(() => _locationName = 'Enable GPS');
+      await Geolocator.openLocationSettings();
+      return false;
+    }
+
+    var permission = await Geolocator.checkPermission();
+
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        setState(() => _locationName = 'Location blocked');
-        return;
-      }
+    }
+
+    if (permission == LocationPermission.denied) {
+      setState(() => _locationName = 'Location denied');
+      return false;
     }
 
     if (permission == LocationPermission.deniedForever) {
-      setState(() => _locationName = 'Location blocked');
-      return;
+      setState(() => _locationName = 'Open settings to enable location');
+      await Geolocator.openAppSettings();
+      return false;
     }
 
-    final position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-
-    _updateLocation(position);
+    return true;
   }
 
-  void _updateLocation(Position position) async {
-    final latLng = LatLng(position.latitude, position.longitude);
+  void _updateLocation(Position pos) async {
+    final latLng = LatLng(pos.latitude, pos.longitude);
+
     setState(() => _currentPosition = latLng);
     _mapController.move(latLng, 17);
 
-    // all 10 seconds we update the "header" with the current location name
     if (_lastLocationUpdate == null || DateTime.now().difference(_lastLocationUpdate!) > const Duration(seconds: 1)) {
       _lastLocationUpdate = DateTime.now();
-      final placemarks = await placemarkFromCoordinates(latLng.latitude, latLng.longitude);
-      if (placemarks.isNotEmpty) {
-        setState(() => _locationName = placemarks.first.locality ?? 'Unknown');
+
+      final place = await placemarkFromCoordinates(latLng.latitude, latLng.longitude);
+
+      if (place.isNotEmpty) {
+        setState(() => _locationName = place.first.locality ?? "Unknown");
       }
     }
   }
@@ -96,33 +114,39 @@ class _MapPageState extends State<MapPage> {
         children: [
           FlutterMap(
             mapController: _mapController,
-            options: MapOptions(initialCenter: _currentPosition ?? const LatLng(46.948, 7.4474), initialZoom: 15),
+            options: MapOptions(
+              initialCenter: _currentPosition ?? const LatLng(46.948, 7.4474), // Bern
+              initialZoom: 15,
+            ),
             children: [
               TileLayer(
                 urlTemplate: _urlTemplate,
                 userAgentPackageName: 'ch.m335.walkeroo',
                 tileProvider: _store.getTileProvider(),
               ),
-              if (_routePoints.isNotEmpty)
-                PolylineLayer(polylines: [Polyline(points: _routePoints, color: Colors.blueAccent, strokeWidth: 3)]),
+
               if (TrackingController().routePoints.isNotEmpty)
                 PolylineLayer(
                   polylines: [
                     Polyline(points: TrackingController().routePoints, color: Colors.blueAccent, strokeWidth: 5),
                   ],
                 ),
+
               if (TrackingController().stopPoints.isNotEmpty)
                 MarkerLayer(
                   markers:
-                      TrackingController().stopPoints.map((p) {
-                        return Marker(
-                          point: p,
-                          width: 20,
-                          height: 20,
-                          child: const Icon(Icons.circle, color: Colors.red, size: 14),
-                        );
-                      }).toList(),
+                      TrackingController().stopPoints
+                          .map(
+                            (p) => Marker(
+                              point: p,
+                              width: 20,
+                              height: 20,
+                              child: const Icon(Icons.circle, color: Colors.red, size: 14),
+                            ),
+                          )
+                          .toList(),
                 ),
+
               if (_currentPosition != null)
                 MarkerLayer(
                   markers: [
@@ -136,6 +160,7 @@ class _MapPageState extends State<MapPage> {
                 ),
             ],
           ),
+
           Positioned(
             top: MediaQuery.of(context).padding.top + 10,
             left: 16,
@@ -148,7 +173,7 @@ class _MapPageState extends State<MapPage> {
                 border: Border.all(color: Colors.white12),
               ),
               child: Text(
-                'Current location - $_locationName',
+                'Current location – $_locationName',
                 textAlign: TextAlign.center,
                 style: const TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.w600),
               ),
